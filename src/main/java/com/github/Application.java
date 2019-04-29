893292f40cd93a9649517bf09bb2567a271c4012
liukaihua <1275820722@qq.com>
2019-04-29 21:05:35 +0800

    发发发

--- src/main/java/com/github/Application.java
+++ src/main/java/com/github/Application.java
@@ -15,6 +15,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
-        System.out.println("");
+        System.out.println("666");
     }
 }
